9518efabd20b5a52f72585560c483be1330afb26
Edna Torres Munill <ethra822@gmail.com>
2024-09-10 15:12:38 +0000

    add functionality to delete items from bag, and update them correctly

--- bag/views.py
+++ bag/views.py
@@ -15,13 +15,9 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
-    quantity = int(request.POST.get('quantity'))
     service = get_object_or_404(IndivService, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    # size = None
-    # if 'product_size' in request.POST:
-    #     size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -38,13 +34,12 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
     service = get_object_or_404(IndivService, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
 
-    if quantity > 0:
+    if quantity >= 1:
         bag[item_id] = quantity
     else:
         bag.pop(item_id)
@@ -56,18 +51,19 @@ def adjust_bag(request, item_id):
 
 def remove_bag_item(request, item_id):
     """Remove the item from the shopping bag"""
-
     try:
-        service = get_object_or_404(Product, pk=item_id)
+        service = get_object_or_404(IndivService, pk=item_id)
         bag = request.session.get('bag', {})
 
-
-        bag.pop(item_id)
+        # Remove the item from the bag
+        bag.pop(item_id)  # Use .pop() with a default to avoid KeyError if item_id is not found
         messages.success(request, f'Removed {service.name} from your bag')
 
+        # Update the session
         request.session['bag'] = bag
-        return HttpResponse(status=200)
+
+        return HttpResponse(status=200)  # Return success status
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+        return HttpResponse(status=500)  # Return error status
