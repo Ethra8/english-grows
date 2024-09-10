ceabebcea5c23957a20afb6641b7a4b71944d1af
Edna Torres Munill <ethra822@gmail.com>
2024-09-10 01:12:34 +0000

    fix bug - mobile-top-header was not showing in md

--- bag/views.py
+++ bag/views.py
@@ -54,28 +54,20 @@ def adjust_bag(request, item_id):
     return redirect(reverse('bag'))
 
 
-# def remove_from_bag(request, item_id):
-#     """Remove the item from the shopping bag"""
+def remove_bag_item(request, item_id):
+    """Remove the item from the shopping bag"""
 
-#     try:
-#         product = get_object_or_404(Product, pk=item_id)
-#         size = None
-#         if 'product_size' in request.POST:
-#             size = request.POST['product_size']
-#         bag = request.session.get('bag', {})
+    try:
+        service = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
 
-#         if size:
-#             del bag[item_id]['items_by_size'][size]
-#             if not bag[item_id]['items_by_size']:
-#                 bag.pop(item_id)
-#                 messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
-#         else:
-#             bag.pop(item_id)
-#             messages.success(request, f'Removed {product.name} from your bag')
 
-#         request.session['bag'] = bag
-#         return HttpResponse(status=200)
+        bag.pop(item_id)
+        messages.success(request, f'Removed {service.name} from your bag')
 
-#     except Exception as e:
-#         messages.error(request, f'Error removing item: {e}')
-#         return HttpResponse(status=500)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
