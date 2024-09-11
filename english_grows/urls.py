9ea37578c7d2a03b4580ab5eb404f2982294f2f2
Edna Torres Munill <ethra822@gmail.com>
2024-09-11 21:44:12 +0000

    create 404 error page

--- english_grows/urls.py
+++ english_grows/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('individual_services/', include('individual_services.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
