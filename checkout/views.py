1ed922637a6eeedb3dbb08d2872603105a7d2660
Edna Torres Munill <ethra822@gmail.com>
2024-09-11 12:23:44 +0000

    add basic stripe functionality at checkout

--- checkout/views.py
+++ checkout/views.py
@@ -11,10 +11,13 @@ from individual_services.models import IndivService
 # from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
+import stripe
 import json
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
@@ -27,6 +30,8 @@ def checkout(request):
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
             order.save()
             for item_id, item_data in bag.items():
@@ -61,15 +66,24 @@ def checkout(request):
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
         order_form = OrderForm()
-
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Pts5fFCegRY4n0nrTvQpQsbUKPQaY6vOYPEBl3tRcJpAzEHxvZfbVTVh5QOfEi2V6xpOuMZ6yNywGVAc9ki0byJ00Sr8FApZP',
-        'client_secret': 'client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
