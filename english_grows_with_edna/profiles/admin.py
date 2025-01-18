63d490866b6a4df0921fbdeea862904a689067a8
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2025-01-18 03:13:29 +0100

    RelatedObjectLookups

---
+++ english_grows_with_edna/profiles/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('user', 'default_full_name', 'default_email',
+                       'default_phone_number')
+
+    fields = ('default_full_name', 'default_email',
+              'default_phone_number', 'default_country')
+
+    list_display = ('user', 'default_full_name', 'default_email',
+                    'default_phone_number', 'default_country')
+
+    ordering = ('-user',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
