68bb722cb61eb545195c2bfa5f0e00fd12462c24
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2025-11-23 15:33:41 +0100

    update to django-crispy-forms >= 2.3

--- home/models.py
+++ home/models.py
@@ -68,7 +68,7 @@ class EmailTemplate(models.Model):
     """
     name = models.CharField(max_length=100, unique=True)
     subject = models.CharField(max_length=200)
-    content = models.CharField()
+    content = models.TextField()
 
     def __str__(self):
         return self.name
